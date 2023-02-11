fa4e4bf39121a48fd90be16aadef27b94ae10911
K <bulat9800@gmail.com>
2023-02-11 22:13:44 +0100

    5

--- app/src/main/java/com/example/kotlinlogin/GodzinyActivity.kt
+++ app/src/main/java/com/example/kotlinlogin/GodzinyActivity.kt
@@ -2,10 +2,24 @@ package com.example.kotlinlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class GodzinyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_godziny)
     }
+
+
+    fun Rezerwuj(v: View) {
+        val button = findViewById<Button>(R.id.button1)
+        //to tez w zaleznosci czy jest juz zarezerwowane czy nie
+        Toast.makeText(this, "Zarezerwowane!", Toast.LENGTH_LONG).show()
+        // zmienianie koloru + zmienianie tekstu w zaleznosci od tego czy zarezerwowane czy nie
+        //button.setBackgroundColor()
+        //button.setText("")
+    }
+
 }
